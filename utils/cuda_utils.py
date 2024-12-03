b79a8304cb90cfc53986bc33d15881398df5a7c0
Kafka Tamura <sajon.tahsen@gmail.com>
2024-12-03 15:33:35 +0600

    fixed cuda compiler typo

--- utils/cuda_utils.py
+++ utils/cuda_utils.py
@@ -6,6 +6,6 @@ def load_cuda(cuda_src, cpp_src, funcs, opt=True, verbose=False):
         cpp_sources=[cpp_src],
         cuda_sources=[cuda_src],
         functions=funcs,
-        extra_cuda_cflags=["-01"] if opt else [],
-        verbose=verbose
+        extra_cuda_cflags=["-O1"] if opt else [],
+        verbose=verbose,
     )
